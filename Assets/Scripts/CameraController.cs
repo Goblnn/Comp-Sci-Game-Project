7e555619a676801181cd68cf31e182620d2cf166
Goblnn <caasier42@gmail.com>
2023-09-01 00:28:45 -0700

    Upload all Unity files and code

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController player;
+
+    public float moveSpeed = 5;
+
+    private bool isMoving;
+    [SerializeField] private bool isCentering = false;
+
+    [SerializeField] private bool cameraCentered = true;
+
+    public Vector3 targetPos;
+    public Vector2 input;
+    public Vector3 tempPos;
+
+    private void Start()
+    {
+        tempPos = player.transform.position;
+        tempPos.z = -30;
+
+        transform.position = tempPos;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            targetPos = player.transform.position;
+            targetPos.z = -30;
+
+            moveSpeed = 15;
+
+            isCentering = true;
+        }
+
+        if (isCentering && (getPlayerPos().x - .1 <= getCameraPos().x && getCameraPos().x <= getPlayerPos().x + .1) && (getPlayerPos().y - .1 <= getCameraPos().y && getCameraPos().y <= getPlayerPos().y + .1))
+        {
+            isCentering = false;
+            cameraCentered = true;
+
+            moveSpeed = 5;
+        }
+
+        if (!isCentering)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                cameraCentered = false;
+
+                targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+            }
+
+            if (cameraCentered)
+            {
+                targetPos = player.transform.position;
+                targetPos.z = -30;
+            }
+        }
+        
+        targetPos.z = -30;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    public Vector3 getPlayerPos()
+    {
+        return player.transform.position;
+    }
+
+    public Vector3 getCameraPos()
+    {
+        return transform.position;
+    }
+
+    public bool camIsCentering()
+    {
+        return isCentering;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public new CameraController camera;
+
+    public float moveSpeed = 5;
+
+    private bool isMoving;
+
+    public Vector3 targetPos;
+
+    public Vector2 input;
+
+    private void Update()
+    {
+        if (camera.camIsCentering() == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                targetPos.z = transform.position.z;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        }
+    }
+}
