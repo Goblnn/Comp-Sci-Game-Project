d56647fc0e2e22f0fa0a58c3d7158be43a479a4e
Goblnn <94708050+Goblnn@users.noreply.github.com>
2023-09-07 08:04:40 -0700

    Add animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public new CameraController camera;
+    private Animator animator;
 
     public float moveSpeed = 5;
 
-    private bool isMoving;
-
     public Vector3 targetPos;
-
     public Vector2 input;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -24,9 +27,37 @@ public class PlayerController : MonoBehaviour
             {
                 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 targetPos.y += 0.375f;
+
+                setAnimationAngle();
             }
             targetPos.z = -1;
             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
         }
+    }
+
+    private void setAnimationAngle()
+    {
+        if(transform.position.x > targetPos.x)
+        {
+            double xDirection = -1;
+        }
+        else
+        {
+            double xDirection = 1;
+        }
+
+        if (transform.position.y > targetPos.y)
+        {
+            double yDirection = -1;
+        }
+        else
+        {
+            double yDirection = 1;
+        }
+
+        double ChangeInX = Math.Abs(targetPos.x - transform.position.x);
+        double ChangeInY = Math.Abs(targetPos.y - transform.position.y);
+
+
     }
 }
